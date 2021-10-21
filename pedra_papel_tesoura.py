eaf12cac418619836f7e80df420b6cd6f99c3665
unknown <felipemallmann@hotmail.com>
2021-10-20 22:24:08 -0300

    Pedra papel tesoura

---
+++ pedra_papel_tesoura.py
@@ -0,0 +1,44 @@
+import random
+
+perguntar = int(input('''Escolha uma opcao para se jogar: 
+
+[0] Pedra
+[1] Papel
+[2] Tesoura
+ 
+Digite sua escolha: '''))
+computador = random.randint(0,2)
+
+if computador == 0:
+    print('O computador escolheu: Pedra')
+    if perguntar == 0:
+        print("Empate!")
+    elif perguntar == 1:
+        print("Jogador perdeu")
+    elif perguntar == 2:
+        print("Computador venceu")
+    else:
+        print("Operacao invalida")
+
+elif computador == 1:
+    print('O computador escolheu: Papel')
+    if perguntar == 0:
+        print("Computador perdeu")
+    elif perguntar == 1:
+        print("Empate!")
+    elif perguntar == 2:
+        print("Jogador venceu")
+    else:
+        print("Operacao invalida")
+elif computador == 2:
+    print('O computador escolheu: Tesoura')
+    if perguntar == 0:
+        print("Jogador venceu")
+    elif perguntar == 1:
+        print("Computador venceu")
+    elif perguntar == 2:
+        print("Empate!")
+    else:
+        print("Operacao invalida")
+else:
+    print("Operacao invalida")
